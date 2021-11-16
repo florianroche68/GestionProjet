a6548d2378ad4284fa1d905e483c53c1cc5c4be9
florian roche <florianroche68@gmail.com>
2021-11-16 11:45:09 +0100

    fix: web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\EntreprisesController;
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
